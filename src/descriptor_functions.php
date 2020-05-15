d25d7868fca8ed35d74e4ede70cead0725d1e21b
tg666 <tomasglawaty@icloud.com>
2020-05-15 02:37:51 +0200

    Added `w_descriptor_range()` function

--- src/descriptor_functions.php
+++ src/descriptor_functions.php
@@ -9,6 +9,13 @@ if (!function_exists('w_descriptor')) {
 	}
 }
 
+if (!function_exists('w_descriptor_range')) {
+	function w_descriptor_range(int $min, int $max, int $step = 100): SixtyEightPublishers\ImageStorage\Responsive\Descriptor\IDescriptor
+	{
+		return SixtyEightPublishers\ImageStorage\Responsive\Descriptor\WDescriptor::fromRange($min, $max, $step);
+	}
+}
+
 if (!function_exists('x_descriptor')) {
 	function x_descriptor(...$pixelDensities): SixtyEightPublishers\ImageStorage\Responsive\Descriptor\IDescriptor
 	{
